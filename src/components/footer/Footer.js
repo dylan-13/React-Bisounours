3c3c64e6127e2e81f30dca6ed25af171a0fe9fcb
Dylan <dylan.berthier13@gmail.com>
2020-01-12 12:59:32 +0100

    BEST Final push for work

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import '../home/Home.css'
 
 function Footer() {
   return (
-      <>
+    <>
+      <div style={{backgroundColor:'#17B0A7', textAlign:'center', padding: '1px', color:'white'}}>
+        <p>FOOTER A TRAVAILLER</p>
+      </div>
     </>
   );
 }
