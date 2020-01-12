54845e57be687e66f11d9501db919bd0a4a79796
Dylan <dylan.berthier13@gmail.com>
2020-01-12 11:35:54 +0100

    final push for simple react front app

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,22 +11,113 @@ import Groscherie from '../../img/groscherie.gif'
 import Groscopain from '../../img/groscopain.gif'
 import Grognon from '../../img/grognon.gif'
 import './Home.css'
-
 export default class Home extends React.Component {
     state = {
-        open: false
+        open1: false,
+        open2: false,
+        open3: false,
+        open4: false,
+        open5: false,
+        open6: false,
+        open7: false,
+        open8: false,
+        open9: false,
+        open10: false       
     };
 
-    onOpenModal = () => {
+    onOpenModal1 = () => {
+        this.setState({open1: true});
+    };
+
+    onCloseModal1 = () => {
+        this.setState({open1: false});
+    };
+
+    onOpenModal2 = () => {
+        this.setState({open2: true});
+    };
+
+    onCloseModal2 = () => {
+        this.setState({open2: false});   
+    };
+
+    onOpenModal3 = () => {
+        this.setState({open3: true});
+    };
+
+    onCloseModal3 = () => {
+        this.setState({open3: false});
+    };
+
+    onOpenModal4 = () => {
         this.setState({open: true});
     };
 
-    onCloseModal = () => {
+    onCloseModal4 = () => {
         this.setState({open: false});
     };
 
+    onOpenModal5 = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal5 = () => {
+        this.setState({open: false});
+    };
+
+    onOpenModal6 = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal6 = () => {
+        this.setState({open: false});
+    };
+
+    onOpenModal7 = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal7 = () => {
+        this.setState({open: false});
+    };
+
+    onOpenModal8 = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal8 = () => {
+        this.setState({open: false});
+    };
+
+    onOpenModal9 = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal9 = () => {
+        this.setState({open: false});
+    };
+
+    onOpenModal10 = () => {
+        this.setState({open: true});
+    };
+
+    onCloseModal10 = () => {
+        this.setState({open: false});
+    };
+
+
     render() {
-        const {open} = this.state;
+        const {open1} = this.state;
+        const {open2} = this.state;
+        const {open3} = this.state;
+        const {open4} = this.state;
+        const {open5} = this.state;
+        const {open6} = this.state;
+        const {open7} = this.state;
+        const {open8} = this.state;
+        const {open9} = this.state;
+        const {open10} = this.state;
+
 
         return ( 
         <>
@@ -34,8 +125,8 @@ export default class Home extends React.Component {
             <div className="col-2 side left">
                 <div className='bear'>
                     <h2 className='titleBear'>Grostaquin<img src={Grostaquin} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal1}>Plus d'informations</button>
+                    <Modal open={open1} onClose={this.onCloseModal1}>
                         <h2 className='titleBear'>Grostaquin</h2>
                         <p>
                         Nom : Grostaquin ou Wish Bear (Ours des Souhaits) ou Désirours
@@ -54,8 +145,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Grosfarceur<img src={Grosfarceur} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal2}>Plus d'informations</button>
+                    <Modal open={open2} onClose={this.onCloseModal2}>
                         <h2 className='titleBear'>Grosfarceur</h2>
                         <p>
                         Nom : Grosfarceur ou Cheer Bear (Ours acclamé) ou Gailourson
@@ -74,8 +165,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Grosjojo<img src={Grosjojo} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal3}>Plus d'informations</button>
+                    <Modal open={open3} onClose={this.onCloseModal3}>
                         <h2 className='titleBear'>Grosjojo</h2>
                         <p>
                         Nom : Grosjojo ou Funshine Bear (Ours d'amusement qui brille) ou Solours
@@ -94,8 +185,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Grosdodo<img src={Grosdodo} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal4}>Plus d'informations</button>
+                    <Modal open={open4} onClose={this.onCloseModal4}>
                         <h2 className='titleBear'>Grosdodo</h2>
                         <p>
                         Nom : Grosdodo ou Bedtime Bear (Ours de l'heure du coucher) ou Dodonours
@@ -116,8 +207,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Grosgateau<img src={Grosgateau} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal5}>Plus d'informations</button>
+                    <Modal open={open5} onClose={this.onCloseModal5}>
                         <h2 className='titleBear'>Grosgateau</h2>
                         <p>
                         Nom : Grosgâteau ou Birthday Bear (Ours de l'anniversaire) ou Fêtalours
@@ -161,8 +252,8 @@ export default class Home extends React.Component {
             <div className="col-2 side right">
                 <div className='bear'>
                     <h2 className='titleBear'>Grosbisous<img src={Grosbisous} alt=""/></h2>                    
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal6}>Plus d'informations</button>
+                    <Modal open={open6} onClose={this.onCloseModal6}>
                         <h2 className='titleBear'>Grosbisous</h2>
                         <p>
                         Nom : Grosbisous ou Tenderheart Bear (Ours au coeur tendre) ou Dounours
@@ -181,8 +272,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Grosveinard<img src={Grosveinard} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal7}>Plus d'informations</button>
+                    <Modal open={open7} onClose={this.onCloseModal7}>
                         <h2 className='titleBear'>Grosveinard</h2>
                         <p>
                         Nom : Grosveinard ou Goodluck Bear (Ours de la chance) ou Chançour
@@ -201,8 +292,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Groscherie<img src={Groscherie} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal8}>Plus d'informations</button>
+                    <Modal open={open8} onClose={this.onCloseModal8}>
                         <h2 className='titleBear'>groscherie</h2>
                         <p>
                         Nom : Groschéri ou Love-a-lot Bear (Ours qui aime beaucoup) ou Cupinours
@@ -221,8 +312,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Groscopain<img src={Groscopain} alt=""/></h2>                    
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal9}>Plus d'informations</button>
+                    <Modal open={open9} onClose={this.onCloseModal9}>
                         <h2 className='titleBear'>Groscopain</h2>
                         <p>
                         Nom : Groscopain ou Friend Bear (Ours ami) ou Copinours
@@ -241,8 +332,8 @@ export default class Home extends React.Component {
                 </div>
                 <div className='bear'>
                     <h2 className='titleBear'>Grognon<img src={Grognon} alt=""/></h2>
-                    <button className='btn' onClick={this.onOpenModal}>Plus d'informations</button>
-                    <Modal open={open} onClose={this.onCloseModal}>
+                    <button className='btn' onClick={this.onOpenModal10}>Plus d'informations</button>
+                    <Modal open={open10} onClose={this.onCloseModal10}>
                         <h2 className='titleBear'>Grognon</h2>
                         <p>
                         Nom : Grognon ou Grumpy Bear (Ours grognon) ou Grognours
